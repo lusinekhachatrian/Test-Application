4449c162a578ab8356de5e5d22c1bbc1941419fa
Lusine Khachatryan <lusinekhachatryan@Lusines-MacBook-Pro.local>
2019-02-05 00:23:02 +0100

    Changes

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<Animal> Get()
+        {
+            string slackToken = "D9TPiByaGZypjnz1LsgU0IEY";
+
+            //to something with token
+            return new Animal(){ Name="Dog"};
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Animal
+    {
+        public string Name {get; set;}
+        public int Age {get; set;}
+        public double Weight {get; set;}
+    }
+}
